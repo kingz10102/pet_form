f8ee0393dd76ceab90aeb91278d59e9e51368011
Kingzz <krem@Kingzzs-MBP.fios-router.home>
2020-07-14 22:38:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+ 
+render() {
+  return(
+    <div className="web-container">
+      <div className="form-container">
+        <h1>PET-A-FRIEND</h1>
+        <form onSubmit={this.handleFormSubmission} noValidate>
+          <div className='dogName'>
+            <label htmlFor="dogName">Dog Name</label>
+            <input type="text" 
+            className= ""
+            placeholder="Your Friend's Name"
+            type="text"
+            name="dogName"
+            noValidate
+            onChange={this.handleChange}
+            />
+          </div>
+          <div className='dogBreed'>
+            <label htmlFor="dogBreed">Friend's Breed</label>
+            <input type="text" 
+            className= ""
+            placeholder="Your Friend's Breed"
+            type="text"
+            name="dogBreed"
+            noValidate
+            onChange={this.handleChange}
+            />
+          </div>
+          <div className='dogBirthday'>
+            <label htmlFor="dogBirthday">Friend's Birthday</label>
+            <input type="text" 
+            className= ""
+            placeholder="Your Friend's Name"
+            type="text"
+            name="dogBirthday"
+            noValidate
+            onChange={this.handleChange}
+            />
+          </div>
+        </form>
+      </div>
+      </div>
+);
 
+}
+}
 export default App;
